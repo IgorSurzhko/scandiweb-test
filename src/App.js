f59cfe43230a80825e4cc7960e3e0b8db97e0b5e
Igor S <ermygut@gmail.com>
2022-01-10 10:14:07 +0200

    set up and check apollo client graphql

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import './App.css';
 import Cart from './pages/Cart';
 import CategoryPage from './pages/CategoryPage';
 import ProductPage from './pages/ProductPage';
+import { useQuery, gql } from '@apollo/client';
+
+const TECH_CATEGORY = gql`
+	query {
+		category(input: { title: "tech" }) {
+			products {
+				id
+				name
+			}
+		}
+	}
+`;
 
 function App() {
+	const { loading, error, data } = useQuery(TECH_CATEGORY);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :(</p>;
+
+	console.log(data);
+
 	return (
-		<Cart />
+		// <Cart />
 		// <ProductPage />
-		// <CategoryPage />;
+		<CategoryPage />
 	);
 }
 
--- src/components/itemCard/ItemCard.js
+++ src/components/itemCard/ItemCard.js
@@ -5,7 +5,15 @@ export default class ItemCard extends Component {
 	render() {
 		return (
 			<div className="cardBox">
-				<img src={require('../../assets/product card example.png')} alt="" />
+				<img src={require('../../assets/product card example.png')} alt="card_image" />
+				<button>
+					<img
+						className="cart"
+						src={require('../../assets/white pan on a green background.png')}
+						alt="cart"
+					/>
+				</button>
+
 				<p className="itemName">Apollo Running Short</p>
 				<p className="itemPrice">$50.00</p>
 			</div>
