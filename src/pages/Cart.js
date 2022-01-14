7e8e5d800cb0d68dc91776d0de5be04ca46deccc
Igor S <ermygut@gmail.com>
2022-01-14 14:52:47 +0200

    local storage serializer / css fix / plural cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,7 +2,8 @@ import { Component } from 'react';
 import CartItem from '../components/cartItem/CartItem';
 import Header from '../components/header/Header';
 import MainText from '../components/mainText/MainText';
-import ProductContext from '../utils/ProductContext';
+import ProductContext from '../utils/productContext';
+import './cart.css';
 
 export default class Cart extends Component {
 	constructor() {
@@ -23,8 +24,15 @@ export default class Cart extends Component {
 			<div>
 				<Header />
 				<MainText text="Cart" />
-				<CartItem prodProps={this.state.purchasedProd} />
-				{/* TODO: MORE ITEMS COMPONENTS  */}
+				{this.state.purchasedProd.product &&
+					this.state.purchasedProd.product.map(element => (
+						<CartItem key={element.prodId} prodProps={element} />
+					))}
+				{this.state.purchasedProd.product && Object.keys(this.state.purchasedProd.product).length === 0 && (
+					<div className="cart">
+						<p> There is no items in your cart</p>
+					</div>
+				)}
 			</div>
 		);
 	}
