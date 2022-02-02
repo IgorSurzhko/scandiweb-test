2902ce204fa7b54fa3eeaab89a6ec011eae856f5
Igor S <ermygut@gmail.com>
2022-02-02 18:58:14 +0200

    fix beckend

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,36 @@
-import { ApolloServer } from 'apollo-server';
+const express = require('express');
+const path = require('path');
+
+import { ApolloServer } from 'apollo-server-express';
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
-const server = new ApolloServer({
-	typeDefs: typeDefs,
-	resolvers: resolvers
-});
+async function startApolloServer(typeDefs: any, resolvers: any) {
+	const server = new ApolloServer({ typeDefs, resolvers });
+	const app = express();
+	await server.start();
+	server.applyMiddleware({ app, path: '/graphql' });
 
-server.listen().then(({ url }) => {
-	console.log(`🚀  Server ready at ${url}`);
-});
+	app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+	app.get('*', (req: any, res: any) => {
+		res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+	});
+	const cors = require('cors');
+	app.use(cors());
+	app.use(function (req: any, res: any, next: any) {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header(
+			'Access-Control-Allow-Headers',
+			'Origin, X-Requested-With, Content-Type, Accept'
+		);
+		next();
+	});
+
+	const PORT = process.env.PORT || 3000;
+	app.listen(PORT, () => {
+		console.log(`Server is listening on port ${PORT}${server.graphqlPath}`);
+	});
+}
+
+startApolloServer(typeDefs, resolvers);
